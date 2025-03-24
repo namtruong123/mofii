faa40ae691671d288160f55fd2949237d836ce66
minhhieu2502 <64004466+minhhieu698hcm@users.noreply.github.com>
2025-03-24 10:08:19 +0700

    1
    1

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mof86385_mofiivietnam' );
+define( 'DB_NAME', 'mofii' );
 
 /** Database username */
-define( 'DB_USER', 'mof86385_mofii' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Mofii@admin' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
